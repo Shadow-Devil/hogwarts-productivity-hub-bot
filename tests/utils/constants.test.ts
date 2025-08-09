ad6f7a9f41eb1af527e61e6f0d26e072ebdb2914
Felix Schnabel <f.schnabel@tum.de>
2025-08-09 13:47:57 +0200

    Fix tests

--- tests/utils/constants.test.ts
+++ tests/utils/constants.test.ts
@@ -55,52 +55,51 @@ describe("Constants Module", () => {
 
 
 
+});
+
+describe("VisualPatterns", () => {
+  test("should have progress bar configuration", () => {
+    expect(VisualPatterns).toHaveProperty("PROGRESS_BAR");
+    expect(VisualPatterns.PROGRESS_BAR).toHaveProperty("FILLED");
+    expect(VisualPatterns.PROGRESS_BAR).toHaveProperty("EMPTY");
+    expect(VisualPatterns.PROGRESS_BAR).toHaveProperty("DEFAULT_LENGTH");
+
+    expect(typeof VisualPatterns.PROGRESS_BAR.DEFAULT_LENGTH).toBe("number");
+    expect(VisualPatterns.PROGRESS_BAR.DEFAULT_LENGTH).toBeGreaterThan(0);
   });
 
-  describe("VisualPatterns", () => {
-    test("should have progress bar configuration", () => {
-      expect(VisualPatterns).toHaveProperty("PROGRESS_BAR");
-      expect(VisualPatterns.PROGRESS_BAR).toHaveProperty("FILLED");
-      expect(VisualPatterns.PROGRESS_BAR).toHaveProperty("EMPTY");
-      expect(VisualPatterns.PROGRESS_BAR).toHaveProperty("DEFAULT_LENGTH");
+  test("should have separator patterns", () => {
+    expect(VisualPatterns).toHaveProperty("SEPARATORS");
 
-      expect(typeof VisualPatterns.PROGRESS_BAR.DEFAULT_LENGTH).toBe("number");
-      expect(VisualPatterns.PROGRESS_BAR.DEFAULT_LENGTH).toBeGreaterThan(0);
-    });
-
-    test("should have separator patterns", () => {
-      expect(VisualPatterns).toHaveProperty("SEPARATORS");
-
-      const separators = ["DASH", "DOT", "ARROW", "BULLET"];
-      separators.forEach((sep) => {
-        expect(VisualPatterns.SEPARATORS).toHaveProperty(sep);
-        expect(typeof VisualPatterns.SEPARATORS[sep]).toBe("string");
-      });
-    });
-
-    test("should have emphasis patterns", () => {
-      expect(VisualPatterns).toHaveProperty("EMPHASIS");
-
-      const emphasis = ["BOLD", "ITALIC", "CODE", "BLOCK"];
-      emphasis.forEach((emp) => {
-        expect(VisualPatterns.EMPHASIS).toHaveProperty(emp);
-        expect(typeof VisualPatterns.EMPHASIS[emp]).toBe("string");
-      });
+    const separators = ["DASH", "DOT", "ARROW", "BULLET"];
+    separators.forEach((sep) => {
+      expect(VisualPatterns.SEPARATORS).toHaveProperty(sep);
+      expect(typeof VisualPatterns.SEPARATORS[sep]).toBe("string");
     });
   });
 
-  describe("Module Integration", () => {
-    test("should export all required constants", () => {
-      expect(BotColors).toBeDefined();
-      expect(VisualPatterns).toBeDefined();
+  test("should have emphasis patterns", () => {
+    expect(VisualPatterns).toHaveProperty("EMPHASIS");
+
+    const emphasis = ["BOLD", "ITALIC", "CODE", "BLOCK"];
+    emphasis.forEach((emp) => {
+      expect(VisualPatterns.EMPHASIS).toHaveProperty(emp);
+      expect(typeof VisualPatterns.EMPHASIS[emp]).toBe("string");
+    });
+  });
+});
+
+describe("Module Integration", () => {
+  test("should export all required constants", () => {
+    expect(BotColors).toBeDefined();
+    expect(VisualPatterns).toBeDefined();
+  });
+
+  test("should have consistent naming conventions", () => {
+    // All color constants should be UPPER_CASE
+    Object.keys(BotColors).forEach((key) => {
+      expect(key).toMatch(/^[A-Z_]+$/);
     });
 
-    test("should have consistent naming conventions", () => {
-      // All color constants should be UPPER_CASE
-      Object.keys(BotColors).forEach((key) => {
-        expect(key).toMatch(/^[A-Z_]+$/);
-      });
-
-    });
   });
 });
