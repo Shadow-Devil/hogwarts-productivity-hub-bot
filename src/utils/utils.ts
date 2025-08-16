81dd473b4a2a6f98bb0a1689386fab75034acb4d
Felix Schnabel <fschnabel007@gmail.com>
2025-08-16 15:37:46 +0000

    Don't override house with null

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,22 +2,22 @@ import { type GuildMember } from "discord.js";
 import assert from "node:assert/strict";
 import type { House } from "../types.ts";
 
-export function getHouseFromMember(member: GuildMember): House | null {
-    let house: House | null = null;
+export function getHouseFromMember(member: GuildMember): House | undefined {
+    let house: House | undefined = undefined;
     if (member.roles.cache.has(process.env.GRYFFINDOR_ROLE_ID!)) {
-        assert(house === null, `member ${member.user.tag} has multiple house roles: ${member.roles.cache.map(r => r.name).join(", ")}`);
+        assert(house === undefined, `member ${member.user.tag} has multiple house roles: ${member.roles.cache.map(r => r.name).join(", ")}`);
         house = "Gryffindor";
     }
     if (member.roles.cache.has(process.env.SLYTHERIN_ROLE_ID!)) {
-        assert(house === null, `member ${member.user.tag} has multiple house roles: ${member.roles.cache.map(r => r.name).join(", ")}`);
+        assert(house === undefined, `member ${member.user.tag} has multiple house roles: ${member.roles.cache.map(r => r.name).join(", ")}`);
         house = "Slytherin";
     }
     if (member.roles.cache.has(process.env.HUFFLEPUFF_ROLE_ID!)) {
-        assert(house === null, `member ${member.user.tag} has multiple house roles: ${member.roles.cache.map(r => r.name).join(", ")}`);
+        assert(house === undefined, `member ${member.user.tag} has multiple house roles: ${member.roles.cache.map(r => r.name).join(", ")}`);
         house = "Hufflepuff";
     }
     if (member.roles.cache.has(process.env.RAVENCLAW_ROLE_ID!)) {
-        assert(house === null, `member ${member.user.tag} has multiple house roles: ${member.roles.cache.map(r => r.name).join(", ")}`);
+        assert(house === undefined, `member ${member.user.tag} has multiple house roles: ${member.roles.cache.map(r => r.name).join(", ")}`);
         house = "Ravenclaw";
     }
     return house;
