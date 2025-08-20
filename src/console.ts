1516ba93d4aec5ecc8e8d174d46f4d1637a1be09
Felix Schnabel <f.schnabel@tum.de>
2025-08-20 20:06:32 +0200

    Fix metrics endpoint not reachable

--- src/console.ts
+++ src/console.ts
@@ -21,28 +21,28 @@ const SystemDLogPriority = {
 
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-//console.log = (...args: any[]) => {
-//    oldConsole.log(`<${SystemDLogPriority.Info}>`, ...args);
-//}
-//
-//console.error = (...args: any[]) => {
-//    oldConsole.error(`<${SystemDLogPriority.Error}>`, ...args);
-//}
-//
-//console.warn = (...args: any[]) => {
-//    oldConsole.warn(`<${SystemDLogPriority.Warning}>`, ...args);
-//}
-//
-//console.info = (...args: any[]) => {
-//    oldConsole.info(`<${SystemDLogPriority.Info}>`, ...args);
-//}
-//
-//console.debug = (...args: any[]) => {
-//    oldConsole.debug(`<${SystemDLogPriority.Debug}>`, ...args);
-//}
-//
-//console.trace = (...args: any[]) => {
-//    oldConsole.trace(`<${SystemDLogPriority.Debug}>`, ...args);
-//}
+console.log = (...args: any[]) => {
+    oldConsole.log(`<${SystemDLogPriority.Info}>`, ...args);
+}
+
+console.error = (...args: any[]) => {
+    oldConsole.error(`<${SystemDLogPriority.Error}>`, ...args);
+}
+
+console.warn = (...args: any[]) => {
+    oldConsole.warn(`<${SystemDLogPriority.Warning}>`, ...args);
+}
+
+console.info = (...args: any[]) => {
+    oldConsole.info(`<${SystemDLogPriority.Info}>`, ...args);
+}
+
+console.debug = (...args: any[]) => {
+    oldConsole.debug(`<${SystemDLogPriority.Debug}>`, ...args);
+}
+
+console.trace = (...args: any[]) => {
+    oldConsole.trace(`<${SystemDLogPriority.Debug}>`, ...args);
+}
 /* eslint-enable @typescript-eslint/no-unsafe-argument */
 /* eslint-enable @typescript-eslint/no-explicit-any */
