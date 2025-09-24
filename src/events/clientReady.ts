df6fa4f6948b0d50cce04b6b5b550d627350ba2a
Felix Schnabel <f.schnabel@tum.de>
2025-09-24 11:37:56 +0200

    Filter better

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -28,7 +28,7 @@ async function resetNicknameStreaks(client: Client<boolean>) {
         discordId: userTable.discordId,
     }).from(userTable).where(gt(userTable.messageStreak, 0)).then(rows => rows.map(r => r.discordId))
 
-    const members = client.guilds.cache.flatMap(guild => guild.members.cache.filter(member => !discordIds.includes(member.id))).filter(member => member.guild.ownerId !== member.user.id);
+    const members = client.guilds.cache.flatMap(guild => guild.members.cache.filter(member => !discordIds.includes(member.id))).filter(member => member.guild.ownerId !== member.user.id && member.nickname?.match(/⚡\d+$/));
     console.log("Members to reset:", members.map(m => m.user.tag).join(", "));
     for await (const [, member] of members) {
         const newNickname = member.nickname?.replace(/⚡\d+$/, "").trim() || member.user.username;
