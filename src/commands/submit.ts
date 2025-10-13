6eae6bd05a81bb6dfa367041774d8bad7b7cfc6e
Felix Schnabel <f.schnabel@reply.de>
2025-10-13 09:22:23 +0200

    Hide submit command under "testing"

--- src/commands/submit.ts
+++ src/commands/submit.ts
@@ -7,24 +7,28 @@ import {
 
 export default {
   data: new SlashCommandBuilder()
-    .setName("submit")
-    .setDescription("Submit a score")
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName("score")
+    .setName("testing")
+    .setDescription("Testing command")
+    .addSubcommandGroup((subcommand) =>
+      subcommand.setName("submit")
         .setDescription("Submit a score")
-        .addIntegerOption((option) =>
-          option
-            .setName("points")
-            .setDescription("The number of points to submit")
-            .setRequired(true),
+        .addSubcommand((subcommand) =>
+          subcommand
+            .setName("score")
+            .setDescription("Submit a score")
+            .addIntegerOption((option) =>
+              option
+                .setName("points")
+                .setDescription("The number of points to submit")
+                .setRequired(true),
+            )
+            .addAttachmentOption((option) =>
+              option
+                .setName("screenshot")
+                .setDescription("A screenshot of your work")
+                .setRequired(true),
+            ),
         )
-        .addAttachmentOption((option) =>
-          option
-            .setName("screenshot")
-            .setDescription("A screenshot of your work")
-            .setRequired(true),
-        ),
     ),
   async execute(interaction: ChatInputCommandInteraction): Promise<void> {
     const username = interaction.user.username;
